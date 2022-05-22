7c8ee08e25a933839f291ee2bb5e9f4cad7c0c34
Janis Beckedorf <janis@beckfish.de>
2022-05-22 16:34:26 +0200

    Add search to virtuoso

--- src/gesetz/row-virtualizer.tsx
+++ src/gesetz/row-virtualizer.tsx
@@ -1,4 +1,5 @@
-import { Virtuoso } from "react-virtuoso";
+import { MutableRefObject, useEffect, useRef } from "react";
+import { Virtuoso, VirtuosoHandle } from "react-virtuoso";
 import { Norm } from "./modals";
 import { NormView } from "./norm-view";
 
@@ -9,8 +10,32 @@ export const RowVirtualizerDynamic = ({
 	rows: Norm[];
 	normIndex: any;
 }) => {
+	const virtuosoRef: MutableRefObject<VirtuosoHandle | null> = useRef(null);
+	useEffect(() => {
+		const match = rows.findIndex(({ index }) => index === normIndex);
+		if (match !== -1) {
+			virtuosoRef.current?.scrollToIndex({
+				index: match,
+				offset: -32,
+			});
+		}
+	}, [normIndex, rows]);
+
+	if (!rows?.length) {
+		return <></>;
+	}
+
+	let startNormIndex = rows.findIndex(({ index }) => index === normIndex);
+	if (startNormIndex === -1) {
+		startNormIndex = 0;
+	}
 	return (
 		<Virtuoso
+			ref={virtuosoRef}
+			initialTopMostItemIndex={{
+				index: startNormIndex,
+				offset: -32,
+			}}
 			useWindowScroll
 			data={rows}
 			itemContent={(index, item) => <NormView data={item}></NormView>}
