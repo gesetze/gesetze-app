bd438277e459cf084ee636cab30a0bafdd39ca57
Janis Beckedorf <janis@beckfish.de>
2022-05-26 11:44:37 +0200

    refactor gesetzview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { GesetzView } from "./gesetz/gesetz";
+import { GesetzView } from "./gesetz/gesetz-view";
 
 function App() {
 	return (
