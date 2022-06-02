aa0ee62e3b1c0d6cf7feea783235a085d1403f06
Janis Beckedorf <janis@beckfish.de>
2022-06-02 22:21:58 +0200

    optimaize rendering

--- src/gesetz/row-virtualizer.tsx
+++ src/gesetz/row-virtualizer.tsx
@@ -1,9 +1,4 @@
-import { MutableRefObject, useEffect, useRef, useState } from "react";
-import {
-	FlatIndexLocationWithAlign,
-	Virtuoso,
-	VirtuosoHandle,
-} from "react-virtuoso";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { Norm } from "./modals";
 import { NormView } from "./norm-view";
 
@@ -17,38 +12,55 @@ export const RowVirtualizer = ({
 	onTopIndexChange: (index: number) => any;
 }) => {
 	const [normIndex, setNormIndex] = useState(-1);
+	const [isRendering, setIsRendering] = useState(false);
+	const [renderRangeEnd, setRenderRangeEnd] = useState(0);
+
+	function checkTopIndex() {
+		Array.from(document.querySelectorAll<HTMLElement>("[id^=row-]")).every(
+			(elem) => {
+				if (elem.getBoundingClientRect().top + elem.offsetHeight >= 64) {
+					const newTopIndex = Number(
+						elem.getAttribute("id")?.replace("row-", "")
+					);
+					onTopIndexChange(newTopIndex);
+					return false;
+				} else {
+					return true;
+				}
+			}
+		);
+	}
 
 	useEffect(() => {
 		setNormIndex(rows.findIndex(({ index }) => index === normId));
+		setRenderRangeEnd(normIndex + 30);
 	}, [rows, normId]);
 
+	// Scroll
 	useEffect(() => {
-		const elem = document.querySelector<HTMLElement>(`[id=row-${normIndex}]`);
-		if (elem) {
-			elem.scrollIntoView({ block: "start" });
-			window.scrollBy({ top: -64 });
-		}
-	}, [normIndex]);
+		if (rows?.length && normIndex !== -1)
+			setTimeout(() => {
+				const elem = document.querySelector<HTMLElement>(
+					`[id=row-${normIndex}]`
+				);
+				if (elem) {
+					setIsRendering(true);
+					elem.scrollIntoView({ block: "start" });
+					window.scrollBy({ top: -64 });
+					setIsRendering(false);
+				}
+				checkTopIndex();
+			});
+	}, [rows, normIndex]);
 
+	// Update onTopIndexChange when scrolling
 	useEffect(() => {
 		let ticking = false;
 		const onScroll = () => {
-			if (!ticking) {
+			if (!ticking && !isRendering) {
 				ticking = true;
 				setTimeout(() => {
-					Array.from(
-						document.querySelectorAll<HTMLElement>("[id^=row-]")
-					).every((elem) => {
-						if (elem.getBoundingClientRect().top + elem.offsetHeight >= 64) {
-							const newTopIndex = Number(
-								elem.getAttribute("id")?.replace("row-", "")
-							);
-							onTopIndexChange(newTopIndex);
-							return false;
-						} else {
-							return true;
-						}
-					});
+					checkTopIndex();
 					ticking = false;
 				}, 500);
 			}
@@ -57,6 +69,12 @@ export const RowVirtualizer = ({
 		window.addEventListener("scroll", onScroll, { passive: true });
 		return () => window.removeEventListener("scroll", onScroll);
 	}, [rows]);
+
+	useLayoutEffect(() => {
+		setRenderRangeEnd(normIndex + 30);
+	}, [rows, normIndex]);
+
+	const renderedRows = rows.slice(0, renderRangeEnd);
 
 	return (
 		<div
@@ -67,7 +85,7 @@ export const RowVirtualizer = ({
 				marginRight: "auto",
 			}}
 		>
-			{rows.map((row, index) => (
+			{renderedRows.map((row, index) => (
 				<div className="row" id={`row-${index}`} key={index}>
 					<NormView data={row}></NormView>
 				</div>
